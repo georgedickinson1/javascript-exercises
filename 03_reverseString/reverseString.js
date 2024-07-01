a29d50a3730d9f9e07909a9235c91c8325ce8f73
George Dickinson <georgedickinson5291@outlook.com>
2024-07-01 09:34:00 +0100

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        console.log(i);
+        outputString += inputString.charAt(i);
+    }
+    return outputString;
 };
+
+console.log(reverseString("Hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
