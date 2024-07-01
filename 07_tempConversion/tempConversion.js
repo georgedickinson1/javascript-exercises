0b12d59a4b180bfcb9d5a7fdf4a6175c973e5e17
George Dickinson <georgedickinson5291@outlook.com>
2024-07-01 14:30:25 +0100

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let rawNumber = (temperature - 32) / 1.8;
+  return Math.round(rawNumber * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let rawNumber = (temperature * 1.8) + 32;
+  return Math.round(rawNumber * 10) / 10;
 };
 
 // Do not edit below this line
