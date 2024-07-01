c80d0ed766ed174d3143527b6680ef6a02481a12
George Dickinson <georgedickinson5291@outlook.com>
2024-07-01 11:15:29 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    if (typeof numberOne != "number" || typeof numberTwo != "number") {
+        return "ERROR";
+    } 
+    let finalSum = 0;
+    if (numberOne < numberTwo) {
+        for (numberOne; numberOne <= numberTwo; numberOne++) {
+            finalSum += numberOne;
+        };
+    } else {
+        for (numberTwo; numberTwo <= numberOne; numberTwo++) {
+            finalSum += numberTwo;
+        };
+    };
+    if (finalSum > 0) {
+        return finalSum;
+    } else {
+        return "ERROR";
+    }
 };
+
+console.log(sumAll(1, "10"))
 
 // Do not edit below this line
 module.exports = sumAll;
