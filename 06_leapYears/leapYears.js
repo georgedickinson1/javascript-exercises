9c66d112d473e375630271ca5fae14e35751292a
George Dickinson <georgedickinson5291@outlook.com>
2024-07-01 14:15:34 +0100

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        };
+    } else {
+        return false;
+    };
 };
+
+console.log(leapYears(1900))
 
 // Do not edit below this line
 module.exports = leapYears;
