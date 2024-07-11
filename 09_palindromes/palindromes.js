e307fbae0d252052ed0a349ae56f2b6b7706b7ba
George Dickinson <georgedickinson5291@outlook.com>
2024-07-11 16:01:23 +0100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[., \/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    if (word === word.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
