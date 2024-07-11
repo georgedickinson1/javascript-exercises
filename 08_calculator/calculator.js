255bca49bf6fb02bef78765bb49693cd12a1f8d3
George Dickinson <georgedickinson5291@outlook.com>
2024-07-11 15:33:32 +0100

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {
+    return total + current
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => {
+    return total * current
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  let numbers = [];
+	for (let i = n; i > 0; i--) {
+    numbers.push(i);
+  }
+  return numbers.reduce((total, current) => {
+    return total * current;
+  })
 };
 
 // Do not edit below this line
