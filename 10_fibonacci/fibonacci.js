147f84bc816dce8ef87892f1d760007b8fd4cb45
George Dickinson <georgedickinson5291@outlook.com>
2024-07-11 16:54:55 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fib = [0, 1,];
+    for (i = 2; i < n + 1; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+  };
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
