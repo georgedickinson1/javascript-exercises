d78acf94e71155e6fa831bfa44da32e77130397b
George Dickinson <georgedickinson5291@outlook.com>
2024-07-12 11:14:23 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arrObj) {
+    const currentYear = new Date().getFullYear();
+    const newArr = arrObj.map(arr => {
+      if (!arr.hasOwnProperty("yearOfDeath")) {
+        arr.yearOfDeath = currentYear;
+      }
+      return arr;
+    })
+  
+    const newArr2 = newArr.map((arr) => {
+        return {
+          ...arr,
+          age: arr.yearOfDeath - arr.yearOfBirth
+        }
+    })
+  
+    newArr2.sort((a, b) => b.age - a.age);
+    return newArr2[0];
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
